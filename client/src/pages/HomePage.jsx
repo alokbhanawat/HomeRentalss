409f8bd52fb6a1faba0e4123b6d2039c83040b38
alokbhanawat <alokbhanawat@gmail.com>
2025-04-11 15:18:45 +0530

    Initial commit with Client and Server structure

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import Categories from "../components/Categories"
+import Listings from "../components/Listings"
+import Navbar from "../components/Navbar"
+import Slide from "../components/Slide"
+
+
+const HomePage = () => {
+  return (
+    <>
+      <Navbar />
+      <Slide />
+      <Categories/>
+      <Listings/>
+      
+    </>
+  )
+}
+
+export default HomePage
